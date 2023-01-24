d1198332bb1330df486168b7c78822ab6cea4edc
hraman1 <121898602+hraman1@users.noreply.github.com>
2023-01-24 13:22:57 -0600

    Update Data Read.R
    @hjennybo

--- Data Read.R
+++ Data Read.R
@@ -1,4 +1,13 @@
 Bitcoin <- read.csv("/Users/harsharaman/Desktop/archive/Bitcoin.csv")
 Ethereum <- read.csv("/Users/harsharaman/Desktop/archive/Ethereum.csv")
 Dogecoin <- read.csv("/Users/harsharaman/Desktop/archive/Meme Coin/Dogecoin.csv")
-Shiba <- read.csv("/Users/harsharaman/Desktop/archive/Meme Coin/Shiba INU.csv")
+Monacoins <- read.csv("/Users/harsharaman/Desktop/archive/Meme Coin/Monacoins.csv")
+
+test1 <- Bitcoin
+write.csv(test1,"/Users/harsharaman/Desktop/archive/test1.csv")
+test2 <- Ethereum
+write.csv(test2,"/Users/harsharaman/Desktop/archive/test2.csv")
+test3 <- Dogecoin
+write.csv(test3,"/Users/harsharaman/Desktop/archive/test3.csv")
+test4 <- Monacoins
+write.csv(test4,"/Users/harsharaman/Desktop/archive/test4.csv")
